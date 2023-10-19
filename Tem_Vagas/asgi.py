23775b7852be7b8a46cce93a0d0bbda3312e8c1d
Luis Felipe Valadares Pantoja <89617375+FelipeLuis123@users.noreply.github.com>
2023-10-19 20:36:35 -0300

    criação do staticfiles

--- Tem_Vagas/asgi.py
+++ Tem_Vagas/asgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
+
 import os
 
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tem_Vagas.settings')
 
-application = get_asgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
